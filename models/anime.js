91808498d61d352efb06fca055210e918b65e286
Anwar Sh <alvaro.sh03@gmail.com>
2022-08-15 18:06:45 +0500

    Set toJSON for animeSchema

--- models/anime.js
+++ models/anime.js
@@ -23,4 +23,12 @@ const animeSchema = new mongoose.Schema(
     }
 )
 
+animeSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
 module.exports = mongoose.model('Anime', animeSchema)
