1e6afcaaf11fcf0a5c26959c4233e8554ec174e3
Anwar Sh <alvaro.sh03@gmail.com>
2022-09-08 14:54:41 +0500

    Implement user data validation

--- controllers/users.js
+++ controllers/users.js
@@ -2,21 +2,44 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
 
-usersRouter.post('/', async (req, res) => {
-    const { username, name, password } = req.body
+usersRouter.post('/', async (req, res, next) => {
+    try {
+        const { username, name, password } = req.body
 
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(password, saltRounds)
+        if (!password) {
+            return res.status(400).json({
+                error: 'password missing'
+            })
+        }
 
-    const newUser = new User({
-        username,
-        name,
-        passwordHash
-    })
+        if (password.length < 3) {
+            return res.status(400).json({
+                error: 'password shorter than three characters'
+            })
+        }
 
-    await newUser.save()
+        const user = await User.findOne({ username })
+        if (user) {
+            return res.status(400).json({
+                error: 'username already taken'
+            })
+        }
 
-    res.status(201).json(newUser)
+        const saltRounds = 10
+        const passwordHash = await bcrypt.hash(password, saltRounds)
+
+        const newUser = new User({
+            username,
+            name,
+            passwordHash
+        })
+
+        await newUser.save()
+
+        res.status(201).json(newUser)
+    } catch (error) {
+        next(error)
+    }
 })
 
 module.exports = usersRouter
--- models/user.js
+++ models/user.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    name: String,
+    username: {
+        type: String,
+        required: true,
+        minLength: 3
+    },
+    name: {
+        type: String,
+        required: true
+    },
     passwordHash: String,
     anime: [
         {
