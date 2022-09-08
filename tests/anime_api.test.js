669b84089a883f702eaba15e34e67e9a925253ad
Anwar Sh <alvaro.sh03@gmail.com>
2022-09-08 12:58:46 +0500

    Write tests for POST /api/anime endpoint

--- tests/anime_api.test.js
+++ tests/anime_api.test.js
@@ -45,6 +45,75 @@ describe('when there are initially some animes', () => {
     })
 })
 
+describe('addition of a new anime', () => {
+    test('succeeds with valid data', async () => {
+        const newAnime = {
+            name: 'New Anime',
+            link: 'newurl',
+            watched: false
+        }
+
+        await api
+            .post('/api/anime')
+            .send(newAnime)
+            .expect(201)
+
+        const response = await api.get('/api/anime')
+
+        const animeAtEnd = response.body.map(b => {
+            return {
+                name: b.name,
+                link: b.link,
+                watched: b.watched
+            }
+        })
+
+        expect(response.body).toHaveLength(initialAnime.length + 1)
+        expect(animeAtEnd).toContainEqual(newAnime)
+    })
+
+    test('fails with suitable code if data invalid', async () => {
+        const animeWithoutName = {
+            link: 'newlink',
+            watched: false
+        }
+
+        const animeWithoutLink = {
+            name: 'New Anime',
+            watched: false
+        }
+
+        await api
+            .post('/api/anime')
+            .send(animeWithoutName)
+            .expect(400)
+
+        await api
+            .post('/api/anime')
+            .send(animeWithoutLink)
+            .expect(400)
+
+        const response = await api.get('/api/anime')
+        
+        expect(response.body).toHaveLength(initialAnime.length)
+    })
+
+    test('defaults watched to false if not provided', async () => {
+        const animeWithoutWatched = {
+            name: 'New Anime',
+            link: 'newurl'
+        }
+
+        const response = await api
+            .post('/api/anime')
+            .send(animeWithoutWatched)
+            .expect(201)
+        
+        expect(response.watched).toBeDefined()
+        expect(response.watched).toBeFalsy()
+    })
+})
+
 afterAll(() => {
     mongoose.connection.close()
 })
