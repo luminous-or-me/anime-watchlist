1e4ffaf51d60717d282b195586d6bf9d0b3d34b3
Anwar Sh <alvaro.sh03@gmail.com>
2022-09-09 12:49:42 +0500

    Write tests for POST /api/users/:id endpoint

--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -57,6 +57,111 @@ describe('when there are initially some users', () => {
     })
 })
 
+describe('addition of users', () => {
+    test('succeeds with valid data', async () => {
+        const newUser = {
+            username: 'newusername',
+            name: 'New Full Name',
+            password: 'newpassword'
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(201)
+        
+        const response = await api.get('/api/users')
+
+        const usernames = response.body.map(u => u.username)
+
+        expect(response.body).toHaveLength(initialUsers.length + 1)
+        expect(usernames).toContain(newUser.username)
+    })
+
+    describe('fails with appropriate status code if', () => {
+        test('username, name, or password missing', async () => {
+            const noUsername = {
+                name: 'New Full Name',
+                password: 'newpassword'
+            }
+
+            const noName = {
+                username: 'newusername',
+                password: 'newpassword'
+            }
+
+            const noPassword = {
+                username: 'newusername',
+                name: 'New Full Name'
+            }
+
+            await api
+                .post('/api/users')
+                .send(noUsername)
+                .expect(400)
+
+            await api
+                .post('/api/users')
+                .send(noName)
+                .expect(400)
+            
+            await api
+                .post('/api/users')
+                .send(noPassword)
+                .expect(400)
+            
+            const response = await api.get('/api/users')
+
+            expect(response.body).toHaveLength(initialUsers.length)
+        })
+
+        test('username or password too short', async () => {
+            const shortUsername = {
+                username: 'ne',
+                name: 'New Full Name',
+                password: 'newpassword'
+            }
+
+            const shortPassword = {
+                username: 'newusername',
+                name: 'New Full Name',
+                password: 'ne'
+            }
+
+            await api
+                .post('/api/users')
+                .send(shortUsername)
+                .expect(400)
+            
+            await api
+                .post('/api/users')
+                .send(shortPassword)
+                .expect(400)
+            
+            const response = await api.get('/api/users')
+
+            expect(response.body).toHaveLength(initialUsers.length)
+        })
+
+        test('username not unique', async () => {
+            const newUser = {
+                username: 'newusername',
+                name: 'New Full Name',
+                password: 'newpassword'
+            }
+
+            await api
+                .post('/api/users')
+                .send(newUser)
+                
+            await api
+                .post('/api/users')
+                .send(newUser)
+                .expect(400)
+        })
+    })
+})
+
 afterAll(() => {
     mongoose.connection.close()
 })
